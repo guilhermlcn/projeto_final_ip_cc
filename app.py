c8cc11b1e165cd89cf67a200784bdee876990b66
GuilhermeLuiz <guilhermecordeiro0507@gmail.com>
2025-05-21 21:40:33 -0300

    Commit Initial

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+import csv
+import os
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -11,5 +15,31 @@ def ola():
 def alo():
     return render_template('sobre-equipe.html')
 
-app.run()
+@app.route('/glossario')
+def glossario():
 
+    glossario_de_termos = []
+
+    with open ('bd_glossario.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for t in reader:
+            glossario_de_termos.append(t)
+
+    return render_template('glossario.html', glossario=glossario_de_termos)
+
+
+@app.route('/novo-termo')
+def novo_termo():
+    return render_template('novo-termo.html')
+
+@app.route('/criar_termo', methods=['POST'])
+def criar_termo():
+  termo = request.form['termo']
+  definicao = request.form['definição']
+
+  with open('bd_glossario.csv', 'a', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow([termo, definicao])
+
+  return redirect(url_for('glossario'))
+app.run()
